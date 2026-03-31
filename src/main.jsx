51e345f365e7f8eb80dcd7355b62ab44e351c2c1
krilph <krealf@mail.ru>
2026-03-31 12:57:39 +0300

    feat: added redux-persist for saving data in localStorage

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import {Provider} from "react-redux"
+import { PersistGate } from 'redux-persist/integration/react'
 
 import './index.css'
 import App from './App.jsx'
-import {store} from "./store/index.js";
+import {persistor, store} from "./store/index.js";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      {/* Здесь мы можем передать прелоадер*/}
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </StrictMode>,
 )
--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,24 @@
 import {createStore} from "redux";
 import {rootReducer} from "./root-reducer.js";
+import {persistReducer, persistStore} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 
-const store = createStore(rootReducer,
+// Создаём конфиг persist
+const persistConfig = {
+  key: 'root',
+  storage,
+  // Указываем какие ключи хотим хранить в storage
+  whilelist: ["filters", "positions"],
+  // Указываем какие ключи НЕ хотим хранить в storage
+  // blacklist: [],
+}
+
+// Используем хелпер и создаём persist reducer
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+// Передаём persistedReducer в createStore
+const store = createStore(persistedReducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 export {store}
+export const persistor = persistStore(store)
